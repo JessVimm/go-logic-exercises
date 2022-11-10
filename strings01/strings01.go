33e052d9d617838c517e13ab74324dcca943d595
Jess Vimm <jessvimm@gmail.com>
2022-11-10 15:03:49 -0600

    Added the optimized solution from strings01 problem

--- strings01/strings01.go
+++ strings01/strings01.go
@@ -6,13 +6,19 @@ import (
 )
 
 func StringsProblemOne() {
-	var firstWord string = "Hell"
-	var secondWord string = "Hello#"
+	var firstWord string = "bbbextm"
+	var secondWord string = "bbb#extm"
 
 	if backspaceCompare(firstWord, secondWord) {
 		fmt.Println("They are equal!!!")
 	} else {
 		fmt.Println("They are not equal...")
+	}
+
+	if optimalComparison(firstWord, secondWord) {
+		fmt.Println("They are equal with the second algorithm!")
+	} else {
+		fmt.Println("Not equal with second algorithm...")
 	}
 }
 
@@ -59,4 +65,52 @@ func generateString(str string) string {
 	}
 
 	return string(runeString) // Return rune turned into string
+}
+
+func optimalComparison(s string, t string) bool {
+	p1, p2 := len(s)-1, len(t)-1 // Initializing pointers
+	counter1, counter2, d1, d2 := 0, 0, 0, 0 // Cat counter
+
+	for i := 0; d1 <= len(s) || d2 <= len(t); i++ {
+		// Count '#'
+		p1 = countCats(&p1, &counter1, s)
+		p2 = countCats(&p2, &counter2, t)
+
+		// Compare if both chars are not equal
+		if p1 >= 0 && p2 >= 0 {
+			if s[p1] != t[p2] {
+				return false
+			}
+		} else if (p1 >= 0 && p2 < 0) || (p2 >= 0 && p1 < 0) {
+			return false
+		}
+		// Move left
+		p1--
+		p2--
+		// Update distances
+		d1 = len(s) - p1 
+		d2 = len(t) - p2
+	}
+	// Were equal
+	return true
+}
+
+func countCats(pointer *int, count *int, str string) int {
+	
+	if *pointer < 0 {
+		return *pointer
+	} 
+
+	if str[*pointer] != '#' && *count == 0 {
+		return *pointer
+	} else if str[*pointer] != '#' && *count > 0 {
+		*pointer--
+		*count--
+	} else {
+		// Continue to move one left and decrement '#' count
+		*pointer--
+		*count++
+	}
+	// Repeat until there is 0 count
+	return countCats(pointer, count, str)
 }

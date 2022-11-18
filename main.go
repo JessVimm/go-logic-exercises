8982bd4d4ee9ebfeb110c8459ea6d852334cfaf8
Jess Vimm <jessvimm@gmail.com>
2022-11-18 13:09:05 -0600

    strings02: add lengthOfLongestSubstring function

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	_"github.com/JessVimm/go-logic-exercises/arrays01"
 	_"github.com/JessVimm/go-logic-exercises/arrays02"
 	_"github.com/JessVimm/go-logic-exercises/arrays03"
-	"github.com/JessVimm/go-logic-exercises/strings01"
+	_"github.com/JessVimm/go-logic-exercises/strings01"
+	"github.com/JessVimm/go-logic-exercises/strings02"
 )
 
 func main() {
 	//arrays01.ArraysProblemOne()
 	//arrays02.ArraysProblemTwo()
 	//arrays03.ArraysProblemThree()
-	strings01.StringsProblemOne()
+	//strings01.StringsProblemOne()
+	strings02.StringsProblemTwo()
 }
---
+++ strings02/strings02.go
@@ -0,0 +1,53 @@
+package strings02
+
+import (
+	"fmt"
+)
+
+func StringsProblemTwo() {
+	var str string = "cinturita"
+	numChars := lengthOfLongestSubstring(str)
+	fmt.Printf("Longest substring has %v characters...", numChars)
+}
+
+func lengthOfLongestSubstring(s string) int {
+	maxLength := 1
+	
+	if len(s) > 0 {	
+		for base := 0; base < len(s); base++ {
+			var charsSeen []string
+			charsSeen = append(charsSeen, string(s[base]))
+			baseLength := 1
+			foundSmilarChar := false
+
+			if len(s) - base <= maxLength {
+				break;
+			}
+
+			for p := base + 1; p < len(s); p++ {
+				// Check if current char has aleready been seen
+				for _, c := range charsSeen {
+					if c == string(s[p]) {
+						// Look elsewhere
+						foundSmilarChar = true
+						break;
+					}
+				}
+
+				if foundSmilarChar {
+					break;
+				} else {
+					baseLength++
+					if baseLength > maxLength {
+						maxLength = baseLength
+					}
+					charsSeen = append(charsSeen, string(s[p]))
+				}
+			}
+		} 
+	} else {
+		maxLength = 0
+	}
+
+	return maxLength
+}
